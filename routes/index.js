b8d7b2fa03480c13140c68cf8c37d65a0750f290
GreenNeon <30697186+GreenNeon@users.noreply.github.com>
2018-10-04 20:50:20 +0700

    azzz

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,69 @@
 var express = require('express');
 var router = express.Router();
+var admin = require('firebase-admin');
+var async = require('async');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.post('/', function (req, res, next) {
+  var error = "";
+  if (req.body.password !== "capedeh123")
+    error = "PASSWORD SALAH";
+  else
+    return res.redirect('/dashboard');
 
+  return res.render('index', { error: error });
+});
+
+
+function listAllUsers(nextPageToken, simplecallback) {
+  // List batch of users, 1000 at a time.
+  admin.auth().listUsers(1000, nextPageToken)
+    .then(function (listUsersResult) {
+      var users = [];
+      if (listUsersResult.pageToken) {
+        listUsersResult.users.forEach(function (userRecord) {
+          //console.log("user", userRecord.toJSON());
+          users.push(userRecord.toJSON());
+        });
+        // List next batch of users.
+         listAllUsers(listUsersResult.pageToken);
+      }
+      console.log('total;', users.length);
+    })
+    .catch(function (error) {
+      var users = [];
+      console.log("Error listing users:", error);
+    });
+};
+
+router.get('/dashboard', function (req, res, next) {
+   // List batch of users, 1000 at a time.
+   admin.auth().listUsers(1000, '1')
+   .then(function (listUsersResult) {
+     var users = [];
+     if (listUsersResult.pageToken) {
+       listUsersResult.users.forEach(function (userRecord) {
+         //console.log("user", userRecord.toJSON());
+         users.push(userRecord.toJSON());
+       });
+       console.log('total;', users[1]);
+
+       return res.render('dashboard', { users:  users});
+       // List next batch of users.
+        //listAllUsers(listUsersResult.pageToken);
+     }
+     console.log('total;', users.length);
+
+   })
+   .catch(function (error) {
+     var users = [];
+     console.log("Error listing users:", error);
+     return res.render('dashboard', { users:  users});
+   });
+});
 
 module.exports = router;
